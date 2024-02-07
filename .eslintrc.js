770af8b87195ca47f88fbb1fb5755a8c5cf21a5d
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-02-07 16:52:13 +0100

    loggin, logut, recover done

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['plugin:@next/next/recommended', '@payloadcms'],
-  ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier'],
-  rules: {
-    'no-console': 'off',
-  },
-}
